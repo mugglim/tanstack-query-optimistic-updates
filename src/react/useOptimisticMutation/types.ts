3f6fe7a437e4f8cdc90b296915cc6b99e6848296
Jaeuk Lim(임재욱) <applewodnr@naver.com>
2025-08-02 17:48:30 +0900

    feat: change optimisticUpdatesFn to getOptimisticState

--- src/react/useOptimisticMutation/types.ts
+++ src/react/useOptimisticMutation/types.ts
@@ -1,20 +1,9 @@
 import { QueryKey } from "@tanstack/react-query";
 
 export type OptimisticUpdateOptions<TQueryData = unknown, TVariables = void> = {
-  /**
-   * A query key for optimistic update
-   */
   queryKey: QueryKey;
-  /**
-   * A function returns the resulting optimistic state.
-   */
-  optimisticUpdatesFn: (param: { prevQueryData: TQueryData; variables: TVariables }) => TQueryData;
-  /**
-   * If set to `true`, the query will fetch if the data is stale.
-   *
-   * @default false
-   */
   invalidateQueryOnSuccess?: boolean | (() => boolean);
+  getOptimisticState: (param: { prevQueryData: TQueryData; variables: TVariables }) => TQueryData;
 };
 
 export type OptimisticUpdateContext = {
